a1a9599c214a7687ca28b87e49d40dc83ea28cab
Yasin <49972383+YA30N-Remix@users.noreply.github.com>
2022-12-18 17:20:57 +0330

    Complete grid columns

--- src/components/AbsencesTable.tsx
+++ src/components/AbsencesTable.tsx
@@ -3,7 +3,27 @@ import { Alert } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table'; 
 import { Absence } from '../utils/models';
 
+/**
+ * method to get period time
+ * @returns date and time 
+ */
+const getPeriod = (startDate: string, endDate: string) => {
+  debugger
+    // To set two dates to two variables
+    var date1 = new Date(startDate);
+    var date2 = new Date(endDate);
+ 
+  // To calculate the time difference of two dates
+  var Difference_In_Time = date2.getTime() - date1.getTime();
+    
+  // To calculate the no. of days between two dates
+  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+  return `${Difference_In_Days + 1} Day`;
+}
+
 const AbsencesTable = (data: any) => {
+  
+
 debugger
   return (  
     <>
@@ -11,7 +31,12 @@ debugger
         <thead>
             <tr>
                 <th scope='col'>ID</th> 
-                <th scope='col'>admitterNote</th> 
+                <th scope='col'>Member name</th> 
+                <th scope='col'>Type of absence</th> 
+                <th scope='col'>Period</th> 
+                <th scope='col'>Member note</th> 
+                <th scope='col'>Status</th>
+                <th scope='col'>Admitter note</th>
 
             </tr>
         </thead>
@@ -24,7 +49,12 @@ debugger
         data.data.map((item: Absence) => (
                 <tr>
                     <td>{item.id} </td>
-                    <td>{item.admitterNote} </td>
+                    <td>{item.userId} </td>
+                    <td>{item.type} </td>
+                    <td>{getPeriod(item.startDate, item.endDate)} </td>
+                    <td>{item.memberNote} </td>
+                    <td>{item.confirmedAt ? 'Confirmed' : 'Rejected'} </td>
+                    <td>{item.admitterId ? item.admitterNote : ''} </td>
                 </tr>
             ))
             )}
